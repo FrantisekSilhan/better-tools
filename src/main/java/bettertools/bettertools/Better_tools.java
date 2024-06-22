fd0bb4b16a441e226e818d82b33ae9ce523780f6
František Šilhán <frantisek@slhn.cz>
2024-06-22 19:30:55 +0200

    init commit

---
+++ src/main/java/bettertools/bettertools/Better_tools.java
@@ -0,0 +1,17 @@
+package bettertools.bettertools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Better_tools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
